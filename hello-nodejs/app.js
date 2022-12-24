028d977c4efff720b8d34dc6b1ebf3d45ed14bbc
tarabola <tarabola@gmail.com>
2022-12-24 11:13:59 -0300

    quiz

--- hello-nodejs/app.js
+++ hello-nodejs/app.js
@@ -1,2 +1,7 @@
 console.log("Hello World!\n");
+const a = 'Mg==';
+const b = 'MTA=';
+const a_n = Number(Buffer.from(a, 'base64').toString('ascii'));
+const b_n = Number(Buffer.from(b, 'base64').toString('ascii'));
+console.log(Math.pow(a_n,b_n));
 
